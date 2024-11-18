234bba876600e96e01fe567d2a5eaf855b57f261
mouse <dlfcaroline@gmail.com>
2024-11-18 19:12:22 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const socketIo = require('socket.io');
+
+const server = http.createServer((req,res)=>{
+	res.writeHead(200, {'Content-Type':'text/plain'});
+	res.end('serveur nodejs en fonctionnement');
+});
+
+const io = socketIo(server, {
+	transports: ['websocket', 'polling'],
+	cors: {
+		origin: '*', // url appli
+		methods: ['GET','POST','PUT','DELETE']
+	}
+});
+
+io.on('connection', (socket)=>{
+	console.log('New user socket id' + socket.id);
+
+	socket.on('message', (msg)=>{
+		console.log('Received message from the client : ' + msg);
+	})
+
+	io.emit('message',"A user joined")
+})
+
+server.listen(8080, ()=>{
+	console.log('Server listening on port 8080');
+});
